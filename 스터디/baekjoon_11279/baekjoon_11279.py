9861fe1d95a0fc651071f99c110789a894d3f9da
[gwag-gyeongho] <[gyeonghogwag@naver.com]>
2022-03-28 11:31:41 +0900

    20220328

--- 스터디/baekjoon_11279/baekjoon_11279.py
+++ 스터디/baekjoon_11279/baekjoon_11279.py
@@ -1,14 +1,12 @@
+import heapq as h
 from sys import stdin
 k = int(stdin.readline())
-mHeap=[]
+maxHeap=[]
 for i in range(k):
     num = int(stdin.readline())
-    if num == 0:
-        if len(mHeap)==0:
+    if len(maxHeap)==0 and num==0:
             print(0)
-        else:
-            
-            print(mHeap.pop(0))
+    elif num==0:
+        print(h.heappop(maxHeap)[1])
     else:
-        mHeap.append(num)   
-        mHeap.sort(reverse=True)
+        h.heappush(maxHeap,(-num,num))
