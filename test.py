4e152f8b3b11486cb86174fbd02d5eb569824856
[gwag-gyeongho] <[gyeonghogwag@naver.com]>
2022-03-31 21:14:32 +0900

    20220331

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
-l=[]
-for i in range(9):
-    k=int(input())
-    l.append(k)
+#소수 출력
 
-print(max(l))
-print(l.index(max(l))+1)
+while True:
+    num1=int(input("***첫 번째 숫자를 입력하세요 :"))
+    num2=int(input("***두 번째 숫자를 입력하세요 :"))
+    if num1 >=2:
+        for i in range(num1,num2):
+            a=0
+            for j in range(2,i):
+                if i%j==0:
+                    a+=1
+            if a==0:
+                print(i)
+        break
+    else:
+        print("첫번째 숫자를 1보다 큰 숫자를 입력하세요")
+    
+    
---
+++ 스터디/2주차/baekjoon_1931/baekjoon_1931.py
@@ -0,0 +1,6 @@
+import sys
+from sys import stdin
+k=int(stdin.readline())
+for i in range(k):
+    n, k = map(int,sys.stdin.readline().split())
+    
