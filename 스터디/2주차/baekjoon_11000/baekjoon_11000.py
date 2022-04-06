aa4ce9bc3be5d6e68d4cee816a789823628bb471
[gwag-gyeongho] <[gyeonghogwag@naver.com]>
2022-04-06 09:59:04 +0900

    20220406

---
+++ 스터디/2주차/baekjoon_11000/baekjoon_11000.py
@@ -0,0 +1,20 @@
+import sys
+from sys import stdin
+import heapq as h
+lecture=[]
+k=int(stdin.readline())
+for _ in range(k):
+    lecture.append(list(map(int,sys.stdin.readline().split())))
+lecture.sort()
+ 
+
+classroom=[]
+h.heappush(classroom,lecture[0][1])
+for i in range(1, k): 
+    if lecture[i][0] < classroom[0] :  
+        h.heappush(classroom,lecture[i][1])
+    else:
+        h.heappop(classroom)
+        h.heappush(classroom,lecture[i][1])
+        
+print(len(classroom))
